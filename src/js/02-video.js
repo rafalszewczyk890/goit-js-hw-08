6c9df8c8dc7adf5a0355c14da2e3a2cfa19a8e6b
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-30 15:28:34 +0100

    Implemented throttle and timechange listener

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    console.log('updated');
+  }, 1000)
+);
