2bde958c7ac1afce5413e4a9307880c67c7003fd
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2022-12-30 12:17:51 +0100

    Added 1 task script before refactor

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,18 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  gallery.insertAdjacentHTML(
+    'afterbegin',
+    `<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt="${item.description}"/>
+</a>`
+  );
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
