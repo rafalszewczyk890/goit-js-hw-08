9449e212b9934f1d478acdfda81985de34decb05
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-01 21:08:04 +0100

    Finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,19 @@
 var throttle = require('lodash.throttle');
 const form = document.querySelector('form');
-
 let object = { email: ' ', message: ' ' };
 
 form.addEventListener('input', throttle(saveMessage, 500));
 form.addEventListener('submit', submitHandle);
-
 setDefault();
 
 function saveMessage() {
   object.email = form.elements.email.value;
   object.message = form.elements.message.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(object));
-  console.log(object);
 }
 
 function setDefault() {
   let jsonVal = JSON.parse(localStorage.getItem('feedback-form-state'));
-
   try {
     form.elements.email.value = jsonVal.email;
     form.elements.message.value = jsonVal.message;
@@ -25,7 +21,6 @@ function setDefault() {
     form.elements.email.value = '';
     form.elements.message.value = '';
   }
-  console.log(jsonVal);
 }
 
 function submitHandle(event) {
