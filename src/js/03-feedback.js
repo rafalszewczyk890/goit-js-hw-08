44c28180a4bbb911c6b3ea54f10ee05c179ad1c5
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-01 20:58:58 +0100

    First half of task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,56 +1,36 @@
-var throttle = require('lodash.throttle');
-
 const form = document.querySelector('form');
 
-let object = { email: '', message: '' };
+let object = { email: ' ', message: ' ' };
 
-let jsonVal = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-function setDefault() {
-  form.elements.email.value = jsonVal.email;
-  form.elements.message.value = jsonVal.message;
-}
+form.addEventListener('input', saveMessage);
+// form.addEventListener('submit', submitHandle);
 
 setDefault();
-
-form.addEventListener('input', throttle(saveMessage, 500));
 
 function saveMessage() {
   object.email = form.elements.email.value;
   object.message = form.elements.message.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(object));
+  console.log(object);
 }
 
-// // function updateOutput() {
-// //   let savedItem = localStorage.getItem('feedback-form-state');
-// //   let jsonItem = JSON.parse(savedItem);
-// //   console.log(jsonItem);
-// //   form.elements.email.value = jsonItem[0];
-// // }
+function setDefault() {
+  let jsonVal = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-// form.addEventListener('input', saveMessage);
+  try {
+    form.elements.email.value = jsonVal.email;
+    form.elements.message.value = jsonVal.message;
+  } catch {
+    form.elements.email.value = '';
+    form.elements.message.value = '';
+  }
+  console.log(jsonVal);
+}
 
-// let emailVal = form.elements.email.value;
-// let textVal = form.elements.message.value;
-
-// function saveMessage(event) {
+// function submitHandle(event) {
 //   event.preventDefault();
-
-//   localStorage.setItem('feedback-form-state', {
-//     email: emailVal,
-//     message: textVal,
-//   });
-//   let savedItem = localStorage.getItem('feedback-form-state');
-//   console.log(savedItem);
+//   form.elements.email.value = '';
+//   form.elements.message.value = '';
+//   localStorage.removeItem('feedback-form-state');
+//   console.log(jsonVal);
 // }
-
-// // function saveMessage(event) {
-// //   event.preventDefault();
-// //   let mailValue = form.elements.email.value;
-// //   let textValue = form.elements.message.value;
-// //   const {
-// //     elements: { email, message },
-// //   } = form;
-// //   localStorage.setItem('feedback-form-state', [mailValue, textValue]);
-// //   updateOutput();
-// // }
