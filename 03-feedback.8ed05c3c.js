16f677f9d46a0090e4f757e5df0de33d867b5298
rafalszewczyk890 <109361257+rafalszewczyk890@users.noreply.github.com>
2023-01-01 20:02:05 +0000

    Deploying to gh-pages from @ rafalszewczyk890/goit-js-hw-08@1827d81f89e0d567695fec607d7ea8a7d23ca26c 🚀

--- 03-feedback.8ed05c3c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");let a={email:" ",message:" "};e.addEventListener("input",(function(){a.email=e.elements.email.value,a.message=e.elements.message.value,localStorage.setItem("feedback-form-state",JSON.stringify(a)),console.log(a)})),function(){let a=JSON.parse(localStorage.getItem("feedback-form-state"));try{e.elements.email.value=a.email,e.elements.message.value=a.message}catch{e.elements.email.value="",e.elements.message.value=""}console.log(a)}();
-//# sourceMappingURL=03-feedback.8ed05c3c.js.map
